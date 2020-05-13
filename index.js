7c9dcea0ae268c56b25c0c36a4b6851461b8b61a
Suruthi-vs <62285669+Suruthi-vs@users.noreply.github.com>
2020-05-13 22:38:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const btnrepos=document.getElementById("btnrepos");
 btnrepos.addEventListener("click",getRepos)
 
 async function getRepos(){
+
+    if(document.getElementById("name").value==""){
+        alert("Enter name")
+    }
+    else{
     const name=document.getElementById("name").value;
     const url=`https://api.github.com/users/${name}`
     const response= await fetch(url)
     const result=await response.json()
     
-    var table=document.getElementsByTagName('table')[0];
+    
+    var table=document.getElementById("tab");
     var newRow=table.insertRow(1);
 
     var cell=newRow.insertCell(0);
@@ -17,6 +23,7 @@ async function getRepos(){
     cell.innerHTML=result.public_repos;
     cell1.innerHTML=result.location;
     cell2.innerHTML=result.followers;
-    }
-
+    document.getElementById("user").reset();
+}
+}
     
